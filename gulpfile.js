0d23a5cbf036819bd07b4309ab2be83d0d47c886
Elyse Kolker Gordon <elyse1213+github@gmail.com>
2015-09-24 22:49:39 -0500

    remove tests/extra tasks from gulp for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,6 @@
 var gulp = require('gulp');
 var path = require('path');
-var jasmine = require('gulp-mocha');
-var babel = require('gulp-babel');
 var browserSync = require('browser-sync').create();
-
-gulp.task('build', function(){
-  return gulp.src(['server/components/*.jsx', 'test/*spec.js'])
-      .pipe(babel())
-      .pipe(gulp.dest("./dist"));
-});
-
-gulp.task('test:server', function () {
-    return gulp.src('dist/*spec.js')
-        .pipe(jasmine({
-            reporter: new reporters.TerminalReporter()
-        }));
-});
 
 gulp.task('browser-sync', function () {
   browserSync.init({
